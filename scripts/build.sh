a0cba6b61f11e63ffc6ef74058deaf83fe2bb4cd
farazzshaikh <farazzshaikh@gmail.com>
2021-09-02 00:17:40 +0400

    Fix workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-
 rollup -c
 rm -rf build/types
 mkdir -p example/build
-cat build/MultipassShaderHelper.cdn.js > example/build/MultipassShaderHelper.cdn.js
+cp build/MultipassShaderHelper.cdn.js example/build/MultipassShaderHelper.cdn.js
